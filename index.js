7627fa3021d26134c0d95609db56ec2eb0a91413
John Doe <mohaiminur24@gmail.com>
2023-05-31 11:01:34 +0600

    user delete and update route is complete

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-const express = require('express');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
-require('dotenv').config();
+require("dotenv").config();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-
-
-
 
 const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_pass}@cluster0.85env82.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -19,7 +16,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -28,7 +25,9 @@ async function run() {
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
 
     const database = client.db("bistro-restaurent");
     const foodMenu = database.collection("food-menu");
@@ -37,74 +36,93 @@ async function run() {
     const userscollection = database.collection("users");
 
     //get signle user route is here
-    app.get("/singleuser", async (req,res)=>{
+    app.get("/singleuser", async (req, res) => {
       const useremail = req.query.email;
-      const result = await userscollection.findOne({email: useremail});
+      const result = await userscollection.findOne({ email: useremail });
       res.send(result);
-    })
+    });
 
     // get all user route is here
-    app.get('/getallusers',async(req,res)=>{
+    app.get("/getallusers", async (req, res) => {
       const result = await userscollection.find().toArray();
       res.send(result);
-    })
+    });
 
     // All menu details is here
-    app.get("/allmenusdetails", async(req,res)=>{
-        const result = await foodMenu.find().toArray();
-        res.send(result);
+    app.get("/allmenusdetails", async (req, res) => {
+      const result = await foodMenu.find().toArray();
+      res.send(result);
     });
 
     // Review route is here
-    app.get("/reviews", async(req,res)=>{
-        const result = await reviews.find().toArray();
-        res.send(result);
+    app.get("/reviews", async (req, res) => {
+      const result = await reviews.find().toArray();
+      res.send(result);
     });
 
     // load cart data route is here
-    app.get("/cartdata", async(req,res)=>{
-        const useremail = req.query.email;
-        const query = { userEmail:useremail }
-        const result = await cartcollection.find(query).toArray();
-        res.send(result);
+    app.get("/cartdata", async (req, res) => {
+      const useremail = req.query.email;
+      const query = { userEmail: useremail };
+      const result = await cartcollection.find(query).toArray();
+      res.send(result);
     });
 
     // Cart colletion route is here
-    app.post("/usercart", async (req, res)=>{
+    app.post("/usercart", async (req, res) => {
       try {
         const item = req.body;
         const result = await cartcollection.insertOne(item);
         res.send(result);
       } catch (error) {
         console.log(error);
-      };
+      }
     });
 
     // Delete cart items from cart route is here
-    app.delete("/deletecartitems", async(req,res)=>{
-        const itemID = req.query.id;
-        const query = {_id:new ObjectId(itemID)};
-        const result = await cartcollection.deleteOne(query);
-        res.send(result)
+    app.delete("/deletecartitems", async (req, res) => {
+      const itemID = req.query.id;
+      const query = { _id: new ObjectId(itemID) };
+      const result = await cartcollection.deleteOne(query);
+      res.send(result);
     });
 
     // set newuser route is here
-    app.post("/createnewuser", async(req,res)=>{
+    app.post("/createnewuser", async (req, res) => {
       const useremail = req.query.email;
       const user = req.body;
-      const exgist = await userscollection.findOne({email:useremail})
-      if(exgist){
+      const exgist = await userscollection.findOne({ email: useremail });
+      if (exgist) {
         res.send("already exgist user");
         return;
       }
       const result = await userscollection.insertOne(user);
       res.send(result);
+    });
+
+    // handle user role route is here
+    app.post("/updateuserrole", async (req, res) => {
+      try {
+        const useremail = req.query.email;
+        const newrRole = req.query.role;
+        const query = { email: useremail };
+        const updatedoc = {
+          $set: {
+            role: newrRole,
+          },
+        };
+        const result = await userscollection.updateOne(query, updatedoc);
+        res.send(result);
+      } catch (error) {}
+    });
+
+    // userdelete route is here
+    app.delete("/deletesingleuser", async (req,res)=>{
+      const userid = req.query.id;
+      const query = {_id: new ObjectId(userid)};
+      const result = await userscollection.deleteOne(query);
+      res.send(result);
     })
-    
-    
-
-
-
 
   } finally {
     // Ensures that the client will close when you finish/error
@@ -113,15 +131,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-app.get("/",(req, res)=>{
-    res.send('Birsto resturent is running well');
+app.get("/", (req, res) => {
+  res.send("Birsto resturent is running well");
 });
 
-
-
-
-app.listen(port, ()=>{
-    console.log(`Server is running with ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server is running with ${port}`);
+});
